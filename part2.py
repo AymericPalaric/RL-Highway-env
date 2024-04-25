f91c8752149f53eee2469bea4a2026b3cb5b042c
Alexandre <alexandre.petit@student-cs.fr>
2024-04-25 18:20:26 +0200

    Mise en forme

--- part2.py
+++ part2.py
@@ -12,25 +12,14 @@ import os
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
 
-from config import config
+from config2 import config
 
 # Créer l'environnement en utilisant la configuration fournie
 env = gym.make("racetrack-v0", render_mode="rgb_array")
 env.unwrapped.configure(config)
 
-# cartpole = gym.make("CartPole-v1", render_mode="rgb_array")
 
 # Créer les agents
-class RandomAgent:
-    def __init__(self, observation_space, action_space):
-        self.action_space = action_space
-
-    def get_action(self, state, **kwargs):
-        # Choose a random float between -1 and 1
-        return self.action_space.sample()
-    
-    def update(self, state, action, reward, next_state, done):
-        pass
 
 def eval_agent(agent, env, n_episodes=10):
     env_copy = deepcopy(env)
@@ -50,35 +39,15 @@ def eval_agent(agent, env, n_episodes=10):
         episode_rewards[i] = total_reward
     return episode_rewards
 
-def run_one_episode(env, agent, display = True):
-    display_env = deepcopy(env)
-    state, _ = display_env.reset()
-    done = False
-    total_reward = 0
-    while not done:
-        action = agent.get_action(state)
-        print(display_env.step(action))
-        next_state, reward, terminated, _, info = display_env.step(action)
-        # next_state, reward, done, _, _ = display_env.step(action)
-        total_reward += reward
-        state = next_state
-        done = terminated or info['crashed'] or info['rewards']['collision_reward'] or not info['rewards']['on_road_reward']
-        if display:
-            clear_output(wait=True)
-            plt.imshow(display_env.render())
-            plt.show()
-    if display:
-        display_env.close()
-    print(f"Total reward: {total_reward}")
-
-# agent = RandomAgent(env.observation_space, env.action_space)
-# run_one_episode(env, agent, display=True)
 
 def train(env, agent, n_episodes, eval_every=10, reward_threshold = 300, n_eval = 10):
     total_time = 0
+    best_reward = 0
+    reward_over_time = []
     for ep in range(n_episodes):
         done = False
         state, _ = env.reset()
+        total_reward = 0
         while not done:
             action = agent.get_action(state)
             next_state, reward, terminated, _, info = env.step(action)
@@ -89,22 +58,23 @@ def train(env, agent, n_episodes, eval_every=10, reward_threshold = 300, n_eval 
             state = next_state
             done = terminated or info['crashed'] or info['rewards']['collision_reward'] or not info['rewards']['on_road_reward']
             total_time += 1
-            if ep == n_episodes -1 :
-                clear_output(wait=True)
-                plt.imshow(env.render())
-                plt.show()
-                env.close()
+            total_reward += reward
+        reward_over_time.append(total_reward)
 
         if (ep+1) % eval_every == 0:
             mean_reward = np.mean(eval_agent(agent, env, n_eval))
+            if mean_reward > best_reward:
+                best_reward = mean_reward
+                # Save best model
+                torch.save(agent.policy_net.state_dict(), "reinforcement_batch.pth")
             print(f"Episode {ep+1}, Mean reward: {mean_reward}")
             if mean_reward > reward_threshold:
                 print(f"Solved in {ep} episodes!")
                 break
     print("Finished training")
-    # Save the model
-    # torch.save(agent.policy_net.state_dict(), "reinforcement_batch.pth")
-    return
+    # Save final model
+    # torch.save(agent.policy_net.state_dict(), "reinforcement_batch_final.pth")
+    return reward_over_time
 
 def resize(x, n_action):
     # Resize the action to [-1, 1]
@@ -116,10 +86,10 @@ class Net(nn.Module):
         self.net = nn.Sequential(
             nn.Linear(obs_size, hidden_size),
             nn.ReLU(),
-            # nn.Linear(hidden_size, hidden_size*2),
-            # nn.ReLU(),
-            # nn.Linear(hidden_size*2, hidden_size),
-            # nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
             nn.Linear(hidden_size, n_actions)
         )
     
@@ -161,8 +131,8 @@ class ReinforceSqueleton:
         # Action space = Box(-1, 1, (1,))
 
         obs_size = 288 # self.observation_space.shape = 2*12*12
-        n_actions = 21 # self.action_space.shape
-        hidden_size = 128
+        n_actions = 21 # self.action_space.shape = [-1 ; -0.9 ; ... ; 0.9 ; 1]
+        hidden_size = 256
         
         self.policy_net = Net(obs_size, hidden_size, n_actions)
         self.current_episode = []
@@ -170,9 +140,6 @@ class ReinforceSqueleton:
         self.optimizer = optim.Adam(self.policy_net.parameters(), lr=self.learning_rate)
         self.n_eps = 0
 
-
-# agent = ReinforceSqueleton(env.action_space, env.observation_space, gamma=0.99, episode_batch_size=1, learning_rate=0.01)
-# run_one_episode(env, agent, display=True)
 
 class Reinforce(ReinforceSqueleton):
 
@@ -208,64 +175,13 @@ class Reinforce(ReinforceSqueleton):
             self.current_episode = []
         return
 
-# agent = Reinforce(env.action_space, env.observation_space, gamma=0.95, episode_batch_size=1, learning_rate=0.01)
-# start_time = time.time()
-# train(env, agent, n_episodes=15000, eval_every=50, reward_threshold=200, n_eval=10)
-# end_time = time.time()
-# print(f"Training time: {(end_time - start_time)//60} min" )
-
-class ReinforceBatch(Reinforce):
-    
-    # Update only after a batch of sequence of episodes
-    def __init__(
-            self,
-            action_space,
-            observation_space,
-            gamma,
-            episode_batch_size,
-            learning_rate,
-    ):
-        super().__init__(action_space, observation_space, gamma, episode_batch_size, learning_rate)
-        self.episode_buffer = []
-
-    def update(self, state, action, reward, done, next_state):
-        self.episode_buffer.append((state, action, reward))
-        if done:
-            self.n_eps += 1
-            if self.n_eps % self.episode_batch_size == 0:
-                self.learn_from_buffer()
-                self.episode_buffer = []
-    
-    def learn_from_buffer(self):
-        states, actions, rewards = zip(*self.episode_buffer)
-        returns = self.gradient_returns(rewards, self.gamma)
-        flat_states = torch.tensor(states.flatten()).float()
-        returns = (returns - returns.mean())/ (returns.std() + 1e-9)
-        action_probs = self.policy_net.forward(flat_states)
-        action_probs = action_probs.item()
-        action = action_probs.argmax()
-        action = resize(action, 21)
-        action = action.item()
-        loss = -torch.log(action)*returns
-        loss = loss.mean()
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-        return
-    
-# agent = ReinforceBatch(env.action_space, env.observation_space, gamma=0.99, episode_batch_size=32, learning_rate=0.0005)
-# start_time = time.time()
-# train(env, agent, n_episodes=1000, eval_every=50, reward_threshold=200, n_eval=10)
-# end_time = time.time()
-# print(f"Training time: {(end_time - start_time)//60} min" )
-
 
 class REINFORCEBatch(Reinforce):
     def update(self, state, action, reward, terminated, next_state):
 
         self.current_episode.append((
             torch.tensor(state).unsqueeze(0),
-            torch.tensor([[action]], dtype=torch.int64),
+            torch.tensor([[action]], dtype=torch.float32),
             torch.tensor([reward]),
         )
         )
@@ -291,10 +207,43 @@ class REINFORCEBatch(Reinforce):
                 self.optimizer.step()
                 
                 self.scores = []
+        
+
+def run_agent(agent, env):
+    state, _ = env.reset()
+    done = False
+    full_reward = 0
+    while not done:
+        action = agent.get_action(state)
+        state, reward, terminated, truncated, info = env.step(action)
+        done = terminated or truncated or info['crashed'] or info['rewards']['collision_reward'] or not info['rewards']['on_road_reward']
+        env.render()
+        full_reward += reward
+    print("final reward", full_reward)
+    env.close()
 
 
-agent = REINFORCEBatch(env.action_space, env.observation_space, gamma=0.95, episode_batch_size=32, learning_rate=0.001)
-start_time = time.time()
-train(env, agent, n_episodes=1500, eval_every=50, reward_threshold=200, n_eval=10)
-end_time = time.time()
-print(f"Training time: {(end_time - start_time)//60} min" )
+def main():
+    TRAIN = True
+    agent = REINFORCEBatch(env.action_space, env.observation_space, gamma=0.99, episode_batch_size=32, learning_rate=0.0005)
+    if TRAIN :
+        start_time = time.time()
+        total_reward = train(env, agent, n_episodes=2500, eval_every=50, reward_threshold=200, n_eval=10)
+        end_time = time.time()
+        print(f"Training time: {(end_time - start_time)//60} min" )
+        plt.title("Reward over time")
+        plt.plot(total_reward)
+        plt.show()
+        plt.savefig("reward.png")
+    else:
+        print("loading model")
+        agent.policy_net.load_state_dict(torch.load("reinforcement_batch.pth"))
+    agent.policy_net.eval()
+    
+    run_agent(agent, env)
+
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
